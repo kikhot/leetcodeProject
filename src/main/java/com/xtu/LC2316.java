5e366c020da8bcac62d0e3aeb2bc462cf3848f6d
quixotic1023 <1363762291@qq.com>
2023-10-21 11:09:19 +0800

    LeetCode2316,并查集+路径压缩。

---
+++ src/main/java/com/xtu/LC2316.java
@@ -0,0 +1,65 @@
+package com.xtu;
+
+import java.util.Arrays;
+
+/**
+ * 使用并查询和压缩路径实现
+ */
+public class LC2316 {
+
+    public long countPairs(int n, int[][] edges) {
+        UnionFind unionFind = new UnionFind(n);
+        for (int i = 0; i < edges.length; i++) {
+            unionFind.union(edges[i][0], edges[i][1]);
+        }
+
+        long result = 0;
+        int[] sizes = unionFind.getSizes();
+        for (int i = 0; i < n; i++) {
+            result += n - sizes[unionFind.find(i)];
+        }
+
+        return result >> 1;
+
+    }
+
+    //并查询
+    class UnionFind {
+        public int fa[];
+        public int sizes[];
+
+        public UnionFind(int n) {
+            fa = new int[n];
+            for (int i = 0; i < n; i++) {
+                fa[i] = i;
+            }
+            sizes = new int[n];
+            Arrays.fill(sizes, 1);
+        }
+
+        // 找i的最顶部元素
+        public int find(int i) {
+            if (fa[i] == i) {
+                return i;
+            } else {
+                fa[i] = find(fa[i]);
+                return fa[i];
+            }
+        }
+
+        // 合并元素
+        public void union(int i, int j) {
+            int i_fa = find(i);
+            int j_fa = find(j);
+            if (i_fa != j_fa) {
+                fa[i_fa] = j_fa;
+                sizes[j_fa] += sizes[i_fa];
+            }
+        }
+
+        // 获取sizes
+        public int[] getSizes() {
+            return sizes;
+        }
+    }
+}
