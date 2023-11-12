b544ea416599d27c8fa2434102f78a832e2a301c
quixotic1023 <quixotic1023@163.com>
2023-11-12 11:51:37 +0800

    并查集数据结构

---
+++ src/main/java/com/xtu/UnionFind.java
@@ -0,0 +1,50 @@
+package com.xtu;
+
+/**
+ * 并查集
+ */
+public class UnionFind {
+
+    private int[] father;
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    UnionFind(int n) {
+        this.count = n;
+        this.father = new int[n];
+        for (int i = 0; i < n; i++) {
+            father[i] = i;
+        }
+    }
+
+    /**
+     * 找u对应的根
+     *
+     * @param u 输入一个值
+     * @return 输出所对应的根
+     */
+    public int find(int u) {
+        return u == father[u] ? u : (father[u] = find(father[u]));
+    }
+
+    /**
+     * 将u插入到v中
+     *
+     * @param u
+     * @param v
+     */
+    public void union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        //如果u和v相等，则v本来就在u这个并查集上，不用进行任何操作
+        if (u == v) {
+            return;
+        }
+        father[u] = v;
+        count--;
+    }
+}
