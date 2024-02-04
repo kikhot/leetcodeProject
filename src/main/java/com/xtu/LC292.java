bef1ca4871c560c0d29594acdaa72e8341e9f3b1
Quixote <1363762291@qq.com>
2024-02-04 23:48:40 +0800

    LC 292. Nim 游戏 使用博弈论分析，并归纳出数学规律

---
+++ src/main/java/com/xtu/LC292.java
@@ -0,0 +1,15 @@
+package com.xtu;
+
+/**
+ * LC 292. Nim 游戏
+ *
+ * @author Quixote
+ * @date 2024/2/4
+ */
+
+public class LC292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
