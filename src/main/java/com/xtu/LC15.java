81064a97e60dc85f56a0200c74d6e4d74a61c500
Quixote <1363762291@qq.com>
2024-04-08 19:30:03 +0800

    LC15 双指针

---
+++ src/main/java/com/xtu/LC15.java
@@ -0,0 +1,40 @@
+package com.xtu;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LC15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            System.out.println(nums[i]);
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> list = new LinkedList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    result.add(list);
+                    while (left < right && nums[left] == nums[++left]) ;
+                    while (left < right && nums[right] == nums[--right]) ;
+                } else if (sum < 0) {
+                    while (left < right && nums[left] == nums[++left]) ;
+                } else {
+                    while (left < right && nums[right] == nums[--right]) ;
+                }
+            }
+        }
+        return result;
+    }
+}
