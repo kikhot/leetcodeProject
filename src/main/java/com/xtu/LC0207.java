55f33a7937a9cccb5b1cc29426d20243701f7fc6
Quixote <1363762291@qq.com>
2024-04-08 15:31:03 +0800

    LC0207 直接用HashSet做的

---
+++ src/main/java/com/xtu/LC0207.java
@@ -0,0 +1,39 @@
+package com.xtu;
+
+import java.util.HashSet;
+
+public class LC0207 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            } else {
+                headB = headB.next;
+            }
+        }
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
